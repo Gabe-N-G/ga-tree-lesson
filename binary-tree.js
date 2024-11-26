13918b251d1638145175cf23854041836040a285
gg_pc <gabedotgutierrez@gmail.com>
2024-11-26 12:17:16 -0500

    search made

--- binary-tree.js
+++ binary-tree.js
@@ -30,14 +30,13 @@ class BinaryTree {
             // if the data is already there, get the hell out you're done.
             if (walker.data === data) return;
             // writing out the solution code in the long format.
-            if(data < walker.data){
+            if (data < walker.data){
                 // binary trees are sorted less than left
                 direction = 'left'
             } else if (data > walker.data) {
                 // greater than right
                 direction = 'right'
             }
-            
             //now that we have the direction of where the data should be in, replace the data if it is empty.
             if (!walker[direction]){
                 return (walker[direction]) = node
@@ -45,16 +44,30 @@ class BinaryTree {
                 // if not keep digging
                 walker = walker[direction]
             }
-
-
-
-
         }
-
-
-
     }
     search(val){
+        
+        if (!this.root) return false;
+
+        let walker = this.root
+
+        while (walker){
+            // if the node exists, return true
+            if (walker.data === val) return true
+            // we can do this because this is a BINARY TREE, as in the data would be searchable by doing greater than/less than over and over again (multi line for clarity)
+            if (val < walker.data){
+                // binary trees are sorted less than left
+                walker = walker.left
+            } else if (val > walker.data) {
+                // greater than right
+                walker = walker.right
+            }
+        }
+        // loop exits if it keeps going left/right and hits a null value and so walker === null at that point
+        return false
+
+
         // search the Tree for a node with the given value
         // if the node exists, return true
         // if the node doesn't exist, return false
